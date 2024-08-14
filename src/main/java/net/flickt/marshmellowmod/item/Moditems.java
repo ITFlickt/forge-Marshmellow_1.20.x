f17204d09269af41699a66e41f45a9348df631b9
ITFlickt <hbit2005@yahoo.com>
2024-08-13 22:36:47 -0400

    Updated Items folder

--- src/main/java/net/flickt/marshmellowmod/item/Moditems.java
+++ src/main/java/net/flickt/marshmellowmod/item/Moditems.java
@@ -1,4 +1,12 @@
 package net.flickt.marshmellowmod.item;
 
+import net.flickt.marshmellowmod.MarshMellowMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
 public class Moditems {
+    public static final DeferredRegister<Item> ITems =
+            DeferredRegister.create(ForgeRegistries.ITEMS, MarshMellowMod.MOD_ID);
+
 }
